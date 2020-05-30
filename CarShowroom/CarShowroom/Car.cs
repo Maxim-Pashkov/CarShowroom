76b5054514b59dcc891f5d6904f7d5b5c7a528cb
Maxim Pashkov <max.pashkov.rez@yandex.ru>
2020-05-30 17:34:55 +0400

    Created base models

---
+++ CarShowroom/CarShowroom/Car.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Reflection;
+using System.Text;
+using Xamarin.Forms;
+
+namespace CarShowroom
+{
+    public class Car
+    {
+        public string Brand { get; set; }
+        public string Model { get; set; }
+        public int Year { get; set; }
+
+        public string Photo { get; set; }
+        public ImageSource PhotoSource { get => ImageSource.FromResource("CarShowroom.images." + Photo, typeof(Car).GetTypeInfo().Assembly); }
+
+        public int HorsePower { get; set; }
+
+        public double Price { get; set; }
+
+        public string MainInfo { get => string.Format("{0} {1}({2} л.с. {3} г.в.)", Brand, Model, HorsePower, Year); }
+
+        public string PriceInfo { get => string.Format("Стоимость: {0:0.##} руб.", Price); }
+    }
+}
---
+++ CarShowroom/CarShowroom/TestDrive.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Collections;
+
+namespace CarShowroom
+{
+    public class TestDrive
+    {
+        public string FirstName { get; set; }
+        public string SecondName { get; set; }
+        public string ThirdName { get; set; }
+
+        public string FullName { get => string.Join(" ", new List<string> { FirstName, SecondName, ThirdName }).Trim(); }
+
+        public string PhoneNumber { get; set; }
+
+        public string Email { get; set; }
+
+        public Car Car { get; set; }
+
+        public string Showroom { get; set; }
+
+        public DateTime DateTime { get; set; }
+
+        public DateTime Date { get => DateTime.Equals(DateTime.MinValue) ? DateTime.Today : DateTime; set
+            {
+                DateTime = new DateTime(value.Year, value.Month, value.Day, Time.Hours, Time.Minutes, 0);
+            }
+        }
+
+        public TimeSpan Time { get => DateTime.Equals(DateTime.MinValue) ? new TimeSpan(12, 0, 0) : DateTime.TimeOfDay; set
+            {
+                DateTime = new DateTime(Date.Year, Date.Month, Date.Day, value.Hours, value.Minutes, 0);
+            }
+        }
+    }
+}
