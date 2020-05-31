00a7ba792dcf5879ac262027efc3eff460266ba2
Maxim Pashkov <max.pashkov.rez@yandex.ru>
2020-05-31 19:59:45 +0400

    Created search view and search page

---
+++ CarShowroom/CarShowroom/SearchPage.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace CarShowroom
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class SearchPage : ContentPage
+	{
+		public SearchPage ()
+		{
+			InitializeComponent ();
+		}
+
+        protected override void OnAppearing()
+        {
+            SearchView_Search(string.Empty);
+            
+            base.OnAppearing();
+        }
+
+        private void SearchView_Search(string text)
+        {
+            ListView.ItemsSource = App.Database.GetCars().Where(Car => string.IsNullOrEmpty(text) || Car.Brand.Contains(text) || Car.Model.Contains(text)).ToList();
+        }
+    }
+}
---
+++ CarShowroom/CarShowroom/SearchView.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Xamarin.Forms;
+
+namespace CarShowroom
+{
+    public delegate void SearchViewEventHandler(string text);
+
+    public partial class SearchView : ContentView
+	{
+        public event SearchViewEventHandler Search;
+
+        public SearchView ()
+		{
+            Entry Entry = new Entry { Placeholder = "Введите текст", HorizontalOptions = LayoutOptions.FillAndExpand, VerticalOptions = LayoutOptions.End};
+            Button Button = new Button { Text = "Искать" };
+
+            Button.Clicked += (sender, e) => Search?.Invoke(Entry.Text); ;
+
+            BindingContext = this;
+
+            Content = new StackLayout {
+                Orientation = StackOrientation.Horizontal,
+                Spacing = 10,
+                Children = {
+                    Entry,
+                    Button,
+				}
+			};
+		}
+    }
+}
