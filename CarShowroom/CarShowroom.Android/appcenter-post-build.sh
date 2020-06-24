c294ed080480c77891c28fa316bb17f73a76758f
Maxim Pashkov <max.pashkov.rez@yandex.ru>
2020-06-24 22:17:59 +0400

    changed post build

--- CarShowroom/CarShowroom.Android/appcenter-post-build.sh
+++ CarShowroom/CarShowroom.Android/appcenter-post-build.sh
@@ -1,43 +1,69 @@
 ﻿#!/usr/bin/env bash
-echo "Post build file init"
 
-if [ "$APPCENTER_XAMARIN_CONFIGURATION" == "Debug" ];then
+echo "Post Build Script Started"
 
-    echo "Post Build Script Started"
+#uitest run
+set -e
 
-    SolutionFile=`find "$APPCENTER_SOURCE_DIRECTORY" -name CarShowroom.sln`
-    SolutionFileFolder=`dirname $SolutionFile`
+##
+# Environment variables required in App Center Build
+# https://docs.microsoft.com/en-us/appcenter/build/custom/variables/
+#
+# APPCENTER_TOKEN
+# https://docs.microsoft.com/en-us/appcenter/api-docs/
+#
+##
 
-    UITestProject=`find "$APPCENTER_SOURCE_DIRECTORY" -name CarShowroom.UITests.csproj`
 
-    echo SolutionFile: $SolutionFile
-    echo SolutionFileFolder: $SolutionFileFolder
-    echo UITestProject: $UITestProject
-
-    chmod -R 777 $SolutionFileFolder
-
-    msbuild "$UITestProject" /property:Configuration=$APPCENTER_XAMARIN_CONFIGURATION
-
-    UITestDLL=`find "$APPCENTER_SOURCE_DIRECTORY" -name "CarShowroom.UITests.dll" | grep bin | head -1` 
-    echo UITestDLL: $UITestDLL
-
-    UITestBuildDir=`dirname $UITestDLL`
-    echo UITestBuildDir: $UITestBuildDir
-
-    UITestVersionNumber=`grep '[0-9]' $UITestProject | grep Xamarin.UITest|grep -o '[0-9]\{1,3\}\.[0-9]\{1,3\}\.[0-9]\{1,3\}'`
-    echo UITestVersionNumber: $UITestVersionNumber
-
-    TestCloudExe=`find ~/.nuget | grep test-cloud.exe | grep $UITestVersionNumber | head -1`
-    echo TestCloudExe: $TestCloudExe
-
-    TestCloudExeDirectory=`dirname $TestCloudExe`
-    echo TestCloudExeDirectory: $TestCloudExeDirectory
-
-    APKFile=`find "$APPCENTER_SOURCE_DIRECTORY" -name *.apk | head -1`
-
-    npm install -g appcenter-cli@1.2.2
-
-    appcenter login --token token
-
-    appcenter test run uitest --app "maximpashkov/CarShowroom" --devices "maximpashkov/base" --app-path $APKFile --test-series "master" --locale "ru_RU" --build-dir $UITestBuildDir --uitest-tools-dir $TestCloudExeDirectory --async
+if find $APPCENTER_SOURCE_DIRECTORY -name '*.UITests.csproj';
+then
+	echo "Building UI test projects:"
+	find $APPCENTER_SOURCE_DIRECTORY -name '*.UITests.csproj' -exec msbuild {} \;
+else
+	echo "Can't find UI test project"
+	exit 9999
 fi
+echo "Compiled projects to run UI tests:"
+find $APPCENTER_SOURCE_DIRECTORY -regex '*.bin.*UITests.*\.dll' -exec echo {} \;
+echo "Running test in App Center Test"
+APPPATH=$APPCENTER_OUTPUT_DIRECTORY/*.apk
+BUILDDIR=$APPCENTER_SOURCE_DIRECTORY/*.UITests/bin/Debug/
+UITESTTOOL=$APPCENTER_SOURCE_DIRECTORY/packages/Xamarin.UITest.*/tools
+appcenter test run uitest --app "maximpashkov/CarShowroom" --devices "maximpashkov/base" --test-series --test-series "master" --locale "ru_RU" --app-path $APPPATH --build-dir $BUILDDIR --async --uitest-tools-dir $UITESTTOOL --token $APPCENTER_TOKEN
+
+echo "End UI tests"
+
+#unit tests run
+echo "Start Unit tests"
+# For Xamarin, run all NUnit test projects that have "Test" in the name.
+# The script will build, run and display the results in the build logs.
+
+echo "Found NUnit test projects:"
+find $APPCENTER_SOURCE_DIRECTORY -regex '.*UnitTest.*\.csproj' -exec echo {} \;
+echo
+echo "Building NUnit test projects:"
+find $APPCENTER_SOURCE_DIRECTORY -regex '.*UnitTest.*\.csproj' -exec msbuild {} \;
+echo
+echo "Compiled projects to run NUnit tests:"
+find $APPCENTER_SOURCE_DIRECTORY -regex '.*bin.*UnitTest.*\.dll' -exec echo {} \;
+echo
+echo "Running NUnit tests:"
+find $APPCENTER_SOURCE_DIRECTORY -regex '.*bin.*UnitTest.*\.dll' -exec nunit3-console {} +
+echo
+echo "NUnit tests result:"
+pathOfTestResults=$(find $APPCENTER_SOURCE_DIRECTORY -name 'TestResult.xml')
+cat $pathOfTestResults
+echo
+
+#look for a failing test
+grep -q 'result="Failed"' $pathOfTestResults
+
+if [[ $? -eq 0 ]]
+then 
+echo "A test Failed" 
+exit 1
+else 
+echo "All tests passed" 
+fi
+
+echo "End Unit tests"
