3dc57f30d4b21f5efa1f5fb3de350f8a3ce11008
Maxim Pashkov <max.pashkov.rez@yandex.ru>
2020-06-26 21:02:28 +0400

    swap ui and unit tests

--- CarShowroom/CarShowroom.Android/appcenter-post-build.sh
+++ CarShowroom/CarShowroom.Android/appcenter-post-build.sh
@@ -3,6 +3,44 @@
 echo "Post Build Script Started"
 
 #---------------------------------------
+#unit tests run
+#---------------------------------------
+
+echo "Start Unit tests"
+# For Xamarin, run all NUnit test projects that have "Test" in the name.
+# The script will build, run and display the results in the build logs.
+
+echo "Found NUnit test projects:"
+find $APPCENTER_SOURCE_DIRECTORY -regex '.*UnitTest.*\.csproj' -exec echo {} \;
+echo
+echo "Building NUnit test projects:"
+find $APPCENTER_SOURCE_DIRECTORY -regex '.*UnitTest.*\.csproj' -exec msbuild {} \;
+echo
+echo "Compiled projects to run NUnit tests:"
+find $APPCENTER_SOURCE_DIRECTORY -regex '.*bin.*UnitTest.*\.dll' -exec echo {} \;
+echo
+echo "Running NUnit tests:"
+find $APPCENTER_SOURCE_DIRECTORY -regex '.*bin.*UnitTest.*\.dll' -exec nunit3-console --list-extensions {} +
+echo
+echo "NUnit tests result:"
+pathOfTestResults=$(find $APPCENTER_SOURCE_DIRECTORY -name 'TestResult.xml')
+cat $pathOfTestResults
+echo
+
+#look for a failing test
+grep -q 'result="Failed"' $pathOfTestResults
+
+if [[ $? -eq 0 ]]
+then 
+echo "A test Failed" 
+exit 1
+else 
+echo "All tests passed" 
+fi
+
+echo "End Unit tests"
+
+#---------------------------------------
 #UI tests run
 #---------------------------------------
 ##
@@ -45,41 +83,3 @@ APKFile=`find "$APPCENTER_SOURCE_DIRECTORY" -name *.apk | head -1`
 appcenter test run uitest --app "maximpashkov/CarShowroom" --devices "maximpashkov/base" --test-series "master" --locale "ru_RU" --app-path $APKFile --build-dir $UITestBuildDir --async --uitest-tools-dir $TestCloudExeDirectory --token $APPCENTER_TOKEN
 
 echo "End UI tests"
-
-#---------------------------------------
-#unit tests run
-#---------------------------------------
-
-echo "Start Unit tests"
-# For Xamarin, run all NUnit test projects that have "Test" in the name.
-# The script will build, run and display the results in the build logs.
-
-echo "Found NUnit test projects:"
-find $APPCENTER_SOURCE_DIRECTORY -regex '.*UnitTest.*\.csproj' -exec echo {} \;
-echo
-echo "Building NUnit test projects:"
-find $APPCENTER_SOURCE_DIRECTORY -regex '.*UnitTest.*\.csproj' -exec msbuild {} \;
-echo
-echo "Compiled projects to run NUnit tests:"
-find $APPCENTER_SOURCE_DIRECTORY -regex '.*bin.*UnitTest.*\.dll' -exec echo {} \;
-echo
-echo "Running NUnit tests:"
-find $APPCENTER_SOURCE_DIRECTORY -regex '.*bin.*UnitTest.*\.dll' -exec nunit3-console --list-extensions {} +
-echo
-echo "NUnit tests result:"
-pathOfTestResults=$(find $APPCENTER_SOURCE_DIRECTORY -name 'TestResult.xml')
-cat $pathOfTestResults
-echo
-
-#look for a failing test
-grep -q 'result="Failed"' $pathOfTestResults
-
-if [[ $? -eq 0 ]]
-then 
-echo "A test Failed" 
-exit 1
-else 
-echo "All tests passed" 
-fi
-
-echo "End Unit tests"
